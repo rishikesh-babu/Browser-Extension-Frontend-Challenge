5441d2b439d0195cff64f5a4a800bbc6e4871324
Rishikesh Babu <rishikeshbabu2004@gmail.com>
2026-03-07 22:55:54 +0530

    Add Extension list

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,17 @@
 import React from 'react'
 import Theme from './Components/Theme'
 import Button from './Components/Button'
+import data from './data/data.json'
+import logo from '/assets/images/icons/logo.svg'
 import Card from './Components/Card'
 
 export default function App() {
-
-    const cardDetails = [
-        {
-            image: 'react.svg', 
-            name: 'DevLens', 
-            content: 'Quickly inspect page layouts and visualize element boundaries', 
-        },
-        {
-            image: 'react.svg', 
-            name: 'DevLens', 
-            content: 'Quickly inspect page layouts and visualize element boundaries', 
-        },
-        {
-            image: 'react.svg', 
-            name: 'DevLens', 
-            content: 'Quickly inspect page layouts and visualize element boundaries', 
-        },
-        {
-            image: 'react.svg', 
-            name: 'DevLens', 
-            content: 'Quickly inspect page layouts and visualize element boundaries', 
-        },
-    ]
-
     return (
-        <div className='py-6 min-h-[100dvh] bg-[#060c22]  '>
-            <div className=' mx-auto max-w-3xl '>
+        <div className='px-1 sm:px-3 py-6 min-h-[100dvh] bg-[#060c22]  '>
+            <div className='mx-auto max-w-6xl '>
                 {/* Heading (Nav) */}
-                <div className=' px-3 sm:px-4 py-1 mx-2 bg-gray-800 rounded-xl flex justify-between items-center'>
-                    <div className=' flex items-center gap-3'>
-                        <img src="react.svg" alt="" />
-                        <div className=' text-xl font-bold text-gray-200 '>
-                            Extensions
-                        </div>
-                    </div>
+                <div className=' px-3 sm:px-4 py-1 bg-gray-700 rounded-xl flex justify-between items-center'>
+                    <img src={logo} alt="logo" />
 
                     <div className=''>
                         <Theme />
@@ -47,12 +20,12 @@ export default function App() {
 
                 {/* Extension Heading and Buttons */}
                 <div className='mt-10'>
-                    <div className=' flex flex-col gap-7'>
+                    <div className=' flex flex-col sm:flex-row sm:justify-between sm:items-center gap-7'>
                         <div className=' text-3xl text-center text-gray-200 font-semibold'>
                             Extension List
                         </div>
 
-                        <div className=' flex justify-evenly'>
+                        <div className=' flex justify-evenly sm:gap-5'>
                             <Button name={'All'} />
                             <Button name={'Acitve'} />
                             <Button name={'Inactive'} />
@@ -61,9 +34,9 @@ export default function App() {
                 </div>
 
                 {/* Extension list */}
-                <div>
-                    {cardDetails.map((item, index) => (
-                        <Card image={item.image} name={item.name} content={item.content} key={index} />
+                <div className='my-10 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-3 '>
+                    {data.map((item, index) => (
+                        <Card name={item.name} description={item.description}  logo={item.logo} isActive={item.isActive} key={index} />
                     ))}
                 </div>
             </div>
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 
-export default function Card({ image, name, content }) {
+export default function Card({ logo, name, description, isActive }) {
     return (
-        <div>
-            <img src={image} alt="" />            
-            {name ?? ''}
-            {content ?? ''}
+        <div className='p-3 bg-gray-800 border border-gray-600 rounded-xl flex flex-col gap-8 '>
+            <div className=' flex items-start gap-3 '>
+                <img src={logo} alt="" />
+
+                <div>
+                    <div className=' text-xl font-bold text-white'>
+                        {name}
+                    </div>
+                    <div className=' text-gray-400'>
+                        {description}
+                    </div>
+                </div>
+            </div>
+
+            <div className=' flex justify-between items-center'>
+                <div className='py-2 px-5 font-bold bg-black/10 border rounded-full cursor-pointer '>
+                    Remove
+                </div>
+                <input type="checkbox" checked={isActive ?? false} className='toggle checked:bg-red-700 checked:border-red-700' />
+            </div>
         </div>
     )
 }
