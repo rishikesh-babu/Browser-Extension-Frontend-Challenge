76afe1d4dd6b670818a0ef59c4de78226e222d28
Rishikesh Babu <rishikeshbabu2004@gmail.com>
2026-03-14 13:16:42 +0530

    Updated file path

--- vite.config.js
+++ vite.config.js
@@ -21,12 +21,12 @@ export default defineConfig({
                 start_url: '/',
                 icons: [
                     {
-                        src: '/assets/images/icons/favicon.png',
+                        src: '/assets/images/icons/favicon-192x192.png',
                         sizes: '192x192',
                         type: 'image/png'
                     },
                     {
-                        src: '/assets/images/icons/favicon.png',
+                        src: '/assets/images/icons/favicon-512x512.png',
                         sizes: '512x512',
                         type: 'image/png'
                     }
